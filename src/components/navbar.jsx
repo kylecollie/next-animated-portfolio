858106965b306099a8b29e214f24e00bafbfee24
Kyle Collie <kcollie@gmail.com>
2024-02-10 23:23:37 -0500

    WIP navbar social links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,8 @@
+'use client';
+
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const links = [
     { url: "/", title: "Home" },
@@ -9,29 +13,59 @@ const links = [
 
 const Navbar = () =>
 {
+    const [open, setOpen] = useState(false);
     return (
         <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+            {/* LINKS */}
+            <div className="hidden md:flex gap-4 w-1/3">
+                {links.map(link => (
+                    <Link href={link.url} key={link.title} className="">{link.title}</Link>
+                ))}
+            </div>
             {/* LOGO */}
-            <div className=''>
+            <div className='md:hidden lg:flex w-1/3 justify-center'>
                 <Link href='/' className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white mr-1">Lama</span>
                     <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
                 </Link>
             </div>
+            {/* SOCIAL */}
+            <div className="hidden md:flex gap-4 w-1/3">
+                <Link href={"#"}>
+                    <Image src="/github.png" alt="github logo" width={24} height={24} />
+                </Link>
+                <Link href={"#"}>
+                    <Image src="/dribbble.png" alt="dribble logo" width={24} height={24} />
+                </Link>
+                <Link href={"#"}>
+                    <Image src="/instagram.png" alt="instagram logo" width={24} height={24} />
+                </Link>
+                <Link href={"#"}>
+                    <Image src="/facebook.png" alt="facebook logo" width={24} height={24} />
+                </Link>
+                <Link href={"#"}>
+                    <Image src="/pinterest.png" alt="pinterest logo" width={24} height={24} />
+                </Link>
+                <Link href={"#"}>
+                    <Image src="/linkedin.png" alt="linkedin logo" width={24} height={24} />
+                </Link>
+            </div>
             {/* RESPONSIVE MENU */}
-            <div className=''>
+            <div className='md:hidden'>
                 {/* MENU BUTTON */}
-                <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={() => setOpen(!open)}>
                     <div className="w-10 h-1 bg-white rounded"></div>
                     <div className="w-10 h-1 bg-white rounded"></div>
                     <div className="w-10 h-1 bg-white rounded"></div>
                 </button>
                 {/* MENU LIST */}
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-                    {links.map((link) => (
-                        <Link href={link.url} key={link.title} className="">{link.title}</Link>
-                    ))}
-                </div>
+                {open && (
+                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                        {links.map((link) => (
+                            <Link href={link.url} key={link.title} className="">{link.title}</Link>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
