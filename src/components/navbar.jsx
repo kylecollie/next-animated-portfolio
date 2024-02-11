858ce847662f90cf5771ee5d87a7da27929f561f
Kyle Collie <kcollie@gmail.com>
2024-02-11 07:41:06 -0500

    adds navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { NavLink } from "./navLink";
 
 const links = [
     { url: "/", title: "Home" },
@@ -15,15 +16,15 @@ const Navbar = () =>
 {
     const [open, setOpen] = useState(false);
     return (
-        <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
             {/* LINKS */}
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map(link => (
-                    <Link href={link.url} key={link.title} className="">{link.title}</Link>
+                    <NavLink key={link.title} link={link} />
                 ))}
             </div>
             {/* LOGO */}
-            <div className='md:hidden lg:flex w-1/3 justify-center'>
+            <div className='md:hidden lg:flex w-1/3 lg:justify-center'>
                 <Link href='/' className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white mr-1">Lama</span>
                     <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
